ce8aaaa97c317d5e6596b322a2b273081ce767d7
NewsLyfe <newslyfe.team@gmail.com>
2025-11-17 01:20:47 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:sonarjs/recommended' // Hozzáadva a teljesebb SonarJS integrációhoz
+    'plugin:sonarjs/recommended' 
   ],
   parser: '@typescript-eslint/parser',
   plugins: ['react', 'react-hooks', '@typescript-eslint', 'sonarjs', 'unused-imports'],
