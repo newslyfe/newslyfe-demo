38541563065ea770ff292a0d0c7432d9cd429c73
NewsLyfe <newslyfe.team@gmail.com>
2025-11-17 12:15:01 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,27 @@
 
+import './i18n';
+import * as React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppProviders } from './providers/AppProviders';
+import { ModalProvider } from './components/Modal';
+import { SEOWrapper } from './routing';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  // React.StrictMode is disabled for debugging purposes.
+  <ModalProvider>
+    <AppProviders>
+      <BrowserRouter>
+        <SEOWrapper>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/c/:countrySlug" element={<App />} />
+          </Routes>
+        </SEOWrapper>
+      </BrowserRouter>
+    </AppProviders>
+  </ModalProvider>
+);
