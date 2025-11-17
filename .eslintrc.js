9c467f014371b2d669250b818ba9355d16b71b72
NewsLyfe <newslyfe.team@gmail.com>
2025-11-17 01:19:03 +0100

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,84 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:sonarjs/recommended' // Hozzáadva a teljesebb SonarJS integrációhoz
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'sonarjs', 'unused-imports'],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/ban-ts-comment': [
+      'warn',
+      {
+        'ts-ignore': 'allow-with-description',
+        'ts-nocheck': 'allow-with-description',
+        'ts-expect-error': 'allow-with-description',
+      },
+    ],
+    'sonarjs/cognitive-complexity': ['warn', 30],
+    'complexity': ['warn', 50],
+    'max-depth': ['warn', 4],
+    'max-lines-per-function': 'off',
+    'max-lines': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  overrides: [
+    {
+      files: ['*.cjs'],
+      rules: {
+        '@typescript-eslint/no-require-imports': 'off',
+      },
+    },
+    {
+      files: [
+        'jest.env.setup.js',
+        'jest.env.setup.cjs',
+        'jest.config.js',
+        'jest.config.ts',
+        'babel.config.cjs',
+        'config/config.js',
+      ],
+      rules: {
+        '@typescript-eslint/no-require-imports': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
+};
